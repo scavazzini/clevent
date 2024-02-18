2fa877a4f4bd38d283d3f29408fa282db8f9dbba
Newton Scavazzini <newtonscavazzini@gmail.com>
2024-02-18 23:12:51 +0100

    feat: hide button text when in loading state

--- app/src/main/java/dev/scavazzini/clevent/ui/components/PrimaryButton.kt
+++ app/src/main/java/dev/scavazzini/clevent/ui/components/PrimaryButton.kt
@@ -1,19 +1,15 @@
 package dev.scavazzini.clevent.ui.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,24 +36,18 @@ fun PrimaryButton(
             else -> false
         },
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            if (state == LOADING) {
-                CircularProgressIndicator(
-                    color = MaterialTheme.colors.primary,
-                    modifier = Modifier.size(20.dp)
-                )
-                Spacer(
-                    modifier = Modifier.width(16.dp),
-                )
-            }
-            if (text.isNotBlank()) {
-                Text(
-                    text = text,
-                    fontSize = 14.sp,
-                )
-            }
+        if (state == LOADING) {
+            return@Button CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+
+        if (text.isNotBlank()) {
+            Text(
+                text = text,
+                fontSize = 14.sp,
+            )
         }
     }
 }
@@ -90,7 +80,7 @@ private fun PrimaryButtonDisabledPreview() {
 
 @Preview("Loading with text")
 @Composable
-private fun PrimaryButtonLoadingWithTextPreview() {
+private fun PrimaryButtonLoadingPreview() {
     PrimaryButton(
         text = "Placing order",
         onClick = { },
@@ -100,10 +90,9 @@ private fun PrimaryButtonLoadingWithTextPreview() {
 
 @Preview("Loading without text")
 @Composable
-private fun PrimaryButtonLoadingWithoutTextPreview() {
+private fun PrimaryButtonBlankTextPreview() {
     PrimaryButton(
         text = "",
         onClick = { },
-        state = LOADING,
     )
 }
