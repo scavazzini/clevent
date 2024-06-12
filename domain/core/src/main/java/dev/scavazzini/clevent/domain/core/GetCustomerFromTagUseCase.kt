971a37fecbf3d84672ce8aa33008e2d0814a9c4d
Newton Scavazzini <newtonscavazzini@gmail.com>
2024-06-12 00:33:20 -0300

    fix: decrypt payload before return on GetCustomerFromTagUseCase

--- domain/core/src/main/java/dev/scavazzini/clevent/domain/core/GetCustomerFromTagUseCase.kt
+++ domain/core/src/main/java/dev/scavazzini/clevent/domain/core/GetCustomerFromTagUseCase.kt
@@ -3,20 +3,23 @@ package dev.scavazzini.clevent.domain.core
 import android.content.Intent
 import dev.scavazzini.clevent.data.core.model.Customer
 import dev.scavazzini.clevent.data.core.repository.TagRepository
+import dev.scavazzini.clevent.domain.core.crypto.SymmetricEncryptor
 import dev.scavazzini.clevent.domain.core.serializer.CustomerSerializer
 import javax.inject.Inject
 
 class GetCustomerFromTagUseCase @Inject constructor(
     private val tagRepository: TagRepository,
     private val customerSerializer: CustomerSerializer,
+    private val encryptor: SymmetricEncryptor,
 ) {
-    operator fun invoke(intent: Intent): Customer {
+    suspend operator fun invoke(intent: Intent): Customer {
         val payload = tagRepository.read(intent)
 
         if (payload.all { it == 0x0.toByte() }) {
             return Customer()
         }
 
-        return customerSerializer.deserialize(payload)
+        val decryptedPayload = encryptor.decrypt(payload)
+        return customerSerializer.deserialize(decryptedPayload)
     }
 }
