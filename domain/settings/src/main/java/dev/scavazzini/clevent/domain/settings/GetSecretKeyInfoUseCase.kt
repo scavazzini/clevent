29e0892b44c144204e4e1b25559d895eb0811a41
Newton Scavazzini <newtonscavazzini@gmail.com>
2024-06-15 21:09:25 -0300

    feat: create usecase to import secret key

--- domain/settings/src/main/java/dev/scavazzini/clevent/domain/settings/GetSecretKeyInfoUseCase.kt
+++ domain/settings/src/main/java/dev/scavazzini/clevent/domain/settings/GetSecretKeyInfoUseCase.kt
@@ -1,14 +1,13 @@
 package dev.scavazzini.clevent.domain.settings
 
-import android.security.keystore.KeyInfo
+import dev.scavazzini.clevent.domain.core.crypto.KeyInfo
 import dev.scavazzini.clevent.domain.core.crypto.SecretKeyManager
-import javax.crypto.SecretKey
 import javax.inject.Inject
 
 class GetSecretKeyInfoUseCase @Inject constructor(
     private val secretKeyManager: SecretKeyManager,
 ) {
-    suspend operator fun invoke(secretKey: SecretKey): KeyInfo? {
-        return secretKeyManager.getKeyInfo(secretKey)
+    suspend operator fun invoke(): KeyInfo? {
+        return secretKeyManager.getKeyInfo()
     }
 }
--- domain/settings/src/main/java/dev/scavazzini/clevent/domain/settings/ImportSecretKeyUseCase.kt
+++ domain/settings/src/main/java/dev/scavazzini/clevent/domain/settings/ImportSecretKeyUseCase.kt
@@ -4,10 +4,11 @@ import dev.scavazzini.clevent.domain.core.crypto.SecretKeyManager
 import javax.crypto.SecretKey
 import javax.inject.Inject
 
-class GetSecretKeyUseCase @Inject constructor(
+class ImportSecretKeyUseCase @Inject constructor(
     private val secretKeyManager: SecretKeyManager,
 ) {
-    suspend operator fun invoke(): SecretKey? {
-        return secretKeyManager.getKey()
+    suspend operator fun invoke(keyBytes: ByteArray): SecretKey {
+        return secretKeyManager.importKey(keyBytes)
     }
+
 }
